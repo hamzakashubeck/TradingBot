37afac6eff0c2df828cbe262e69e673690546581
Hamza Kashubeck <93697670+hamzakashubeck@users.noreply.github.com>
2022-07-31 23:07:45 -0400

    Create stock_trader.py

---
+++ stock_trader.py
@@ -0,0 +1,117 @@
+'''
+Created on Jul 29, 2022
+
+@author: hamzakashubeck
+'''
+import talib
+import alpaca_trade_api as api
+from yahooquery import Ticker
+from datetime import datetime
+
+# ---------------- ALPACA HELPER FUNCTIONS HERE -------------------- #
+
+    # submits a market buy order. returns the resulting Alpaca order object
+def market_buy(ticker,qty):
+    return alpaca.submit_order(ticker.upper(), qty, side='buy', type='market')
+    
+    # submits a market sell order. returns the resulting Alpaca order object
+def market_sell(ticker,qty):
+    return alpaca.submit_order(ticker.upper(), qty, side='sell', type='market')
+    
+    # submits a limit buy order. returns the resulting Alpaca order object
+def limit_buy(ticker,qty,limit):
+    return alpaca.submit_order(ticker.upper(), qty, side='buy', type='limit', limit_price=limit)
+    
+    # submits a limit sell order. returns the resulting Alpaca order object
+def limit_sell(ticker,qty,limit):
+    return alpaca.submit_order(ticker.upper(), qty, side='sell', type='limit', limit_price=limit)
+    
+    # submits a trailing stop sell order for a given percent. returns the resulting order object
+def trailing_stop_percent(ticker,qty,percent):
+    return alpaca.submit_order(ticker.upper(), qty, side='sell', type='trailing_stop', trail_percent=percent)
+
+    #retrieves the order status of a given order
+    #Possible statuses: new, filled OR partially_filled, cancelled, expired, and other less common statuses
+def order_status(order_id):
+    #to get an order_id, syntax is orderObj.id
+    order = alpaca.get_order(order_id)
+    return order.status
+    
+# ---------------- STOCK RESEARCH HELPER FUNCTIONS HERE -------------------- #
+
+    #retrieves the current market price from yahoo finance
+def get_price(ticker):
+    return Ticker(ticker).price[ticker]['regularMarketPrice']
+
+    #retrieves the current bid price from yahoo finance
+def get_bid_price(ticker):
+    return Ticker(ticker).summary_detail[ticker]['bid']
+
+    #retrieves the current ask price from yahoo finance
+def get_ask_price(ticker):
+    return Ticker(ticker).summary_detail[ticker]['ask']
+
+    #retrieves the current rsi value calculated from recent minute closing prices from yahoo finance
+def get_rsi_val(ticker):
+    candles = Ticker(ticker).history(period='5d', interval='1m')
+    return talib.stream_RSI(candles.close,timeperiod = 12)
+
+    #retrieves bollinger band values calculated from recent minute closing prices from yahoo finance
+def get_bbands(ticker):
+    candles = Ticker(ticker).history(period='4d', interval='1m')
+    return talib.stream_BBANDS(candles.close)
+
+# --------------------- ALGORITHM/TESTING FUNCTIONS --------------------------- #
+
+    # returns a 0 for inconclusive, 1 for buy signal, 2 for sell signal
+def get_bbands_signal(ticker):
+    price = get_price(ticker)
+    upper, middle, lower = get_bbands(ticker)
+    if price < lower:
+        print('bbands BUY signal at '+str(datetime.now())+ ' at a price of '+str(price))
+        return 1
+    if price > upper:
+        print('bbands SELL signal at '+str(datetime.now())+ ' at a price of '+str(price))
+        return 2
+    return 0
+    
+    #runs the main simulation
+def trade_SPY_test():
+    ticker = 'SPY'
+    
+    last_signal = 0
+    while(True):
+        signal = get_bbands_signal(ticker)
+        
+        if signal == 1:
+            if last_signal!=1: #BUY
+                last_signal = 1
+                try:
+                    limit_buy(ticker,1,get_ask_price(ticker)) #Buy at the current ask price
+                    print('BUY ORDER AT '+str(datetime.now()))
+                except:
+                    print('BUY ORDER AT '+str(datetime.now())+ ', but not enough buying power.')
+                #order will fail if not enough cash available
+                
+        elif signal == 2: #SELL
+            last_signal = 2
+            #qty_held = int(alpaca.get_position(ticker).qty)
+            #if qty_held>0:
+            try:
+                limit_sell(ticker,1,get_bid_price(ticker)) #Sell at the current bid price
+                print('SELL ORDER AT '+str(datetime.now()))
+            except:
+                print('SELL ORDER AT '+str(datetime.now())+ ', but no shares are currently owned.')
+        else:
+            last_signal = 0
+
+# ----------------------- END HELPER FUNCTIONS ----------------------------- #
+
+# The following input values would be specific to my Alpaca account:
+API_KEY = ''
+API_SECRET = ''
+BASE_URL = 'https://paper-api.alpaca.markets'
+
+alpaca = api.REST(API_KEY, API_SECRET, BASE_URL)
+
+trade_SPY_test()
